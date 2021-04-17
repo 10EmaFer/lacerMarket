24e6267c21e99d7053047cae81c9b8ceea200087
10EmaFer <emanueleferrara10.ef@gmail.com>
2021-04-17 11:39:55 +0200

    creata dashboard e storico alla dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,12 @@
 class PagesController < ApplicationController
+	before_action :authenticate_user!, only: [:dashboard]
   def home
   end
   def about
+  end
+  def dashboard
+  	@content = current_user.contents
+  	@sales = Sale.where(email_venditore: current_user.email)
+  	@purchased = Sale.where(email_aquirente: current_user.email)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
 
   post '/compra/:slug', to: 'transaction#create', as: :compra
   get '/pickup/:guid', to: 'transaction#pickup', as: :pickup
+  get 'dashboard' => 'pages#dashboard'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
